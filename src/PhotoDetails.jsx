b3301a59e26b0d2e321599fa9fd1908d989b519e
Willem Lu <willhtun@amazon.com>
2025-01-20 20:13:05 -0800

    Handle browser back button

--- src/PhotoDetails.jsx
+++ src/PhotoDetails.jsx
@@ -1,68 +1,69 @@
 import './PhotoDetails.css'
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
-class PhotoDetails extends React.Component {
-  constructor(props) {
-    super(props);
-    this.photo_url = this.props.photoData["s3_image_url"]
-    this.photo_title = this.props.photoData["title"]
-    this.photo_date = this.props.photoData["date"]
-    this.photo_description = this.props.photoData["description"]
-    this.photo_description_is_poem = this.props.photoData["is_poem"]
-    this.photo_latitude = this.props.photoData["location"]["latitude"]
-    this.photo_longitude = this.props.photoData["location"]["longitude"]
-    this.photo_location_name = this.props.photoData["location"]["name"]
-    this.closePhoto = this.closePhoto.bind(this)
-  }
+function PhotoDetails(props) {
+  const navigate = useNavigate();
+  const photo_url = props.photoData["s3_image_url"]
+  const photo_title = props.photoData["title"]
+  const photo_date = props.photoData["date"]
+  const photo_description = props.photoData["description"]
+  const photo_description_is_poem = props.photoData["is_poem"]
+  const photo_latitude = props.photoData["location"]["latitude"]
+  const photo_longitude = props.photoData["location"]["longitude"]
+  const photo_location_name = props.photoData["location"]["name"]
 
-  windowResized(e) {
-    const handleResize = () => this.setState({
+  const windowResized = (e) => {
+    const handleResize = () => setState({
       isMobile: window.innerWidth < 600? true: false
     });
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }
 
-  closePhoto() {
-    this.props.showPhotoHandler(false)
+  window.onpopstate = () => {
+    props.showPhotoHandler(false)
   }
 
-  render() {
-    return (
-      <div class="photo-details">
-        <div class="photo-details-left-padding">
-          <div class="photo-details-close-wrapper" onClick={this.closePhoto}>
-            <div class="photo-details-close" style={{ backgroundImage:"url(/close.svg)" }}></div>
-          </div>
-        </div>
-        <div class="photo-details-left" style={{ backgroundImage:"url(" + this.photo_url + ")" }}></div>
-        <div class="photo-details-right">
-          <div class="photo-details-title">
-            <h1>{this.photo_title}</h1>
-            <h2>{this.photo_date}</h2>
-          </div>
-          <div class={`photo-details-description${this.photo_description_is_poem? "-poem": ""}`}>
-            <p>{this.photo_description}</p>
-          </div>
-          <iframe 
-            class="photo-details-location"
-            loading="lazy" 
-            src={ "https://www.google.com/maps/embed/v1/place?q=" + this.photo_latitude + "%2C" + this.photo_longitude + "&key=AIzaSyCzAeGno02JZsVHOHo56uZJLvSn0It6ioE" }>
-          </iframe>
-          <div class="photo-details-location-text">
-            <h3>{this.photo_location_name}</h3>
-          </div>
-        </div>
-        <div class="photo-details-right-padding"></div>
-        <div class="photo-details-close-mobile" onClick={this.closePhoto}>
-          <div class="photo-details-close-mobile-icon" style={{ backgroundImage:"url(/close.svg)" }}></div>
-          <div class="photo-details-close-mobile-text">
-            <p>back</p>
-          </div>
-        </div>
-      </div> 
-    )
+  const closePhoto = () => {
+    navigate(-1)
+    props.showPhotoHandler(false)
   }
+
+  return (
+    <div class="photo-details">
+      <div class="photo-details-left-padding">
+        <div class="photo-details-close-wrapper" onClick={closePhoto}>
+          <div class="photo-details-close" style={{ backgroundImage:"url(/close.svg)" }}></div>
+        </div>
+      </div>
+      <div class="photo-details-left" style={{ backgroundImage:"url(" + photo_url + ")" }}></div>
+      <div class="photo-details-right">
+        <div class="photo-details-title">
+          <h1>{photo_title}</h1>
+          <h2>{photo_date}</h2>
+        </div>
+        <div class={`photo-details-description${photo_description_is_poem? "-poem": ""}`}>
+          <p>{photo_description}</p>
+        </div>
+        <iframe 
+          class="photo-details-location"
+          loading="lazy" 
+          src={ "https://www.google.com/maps/embed/v1/place?q=" + photo_latitude + "%2C" + photo_longitude + "&key=AIzaSyCzAeGno02JZsVHOHo56uZJLvSn0It6ioE" }>
+        </iframe>
+        <div class="photo-details-location-text">
+          <h3>{photo_location_name}</h3>
+        </div>
+      </div>
+      <div class="photo-details-right-padding"></div>
+      <div class="photo-details-close-mobile" onClick={closePhoto}>
+        <div class="photo-details-close-mobile-icon" style={{ backgroundImage:"url(/close.svg)" }}></div>
+        <div class="photo-details-close-mobile-text">
+          <p>back</p>
+        </div>
+      </div>
+    </div> 
+  )
 }
 
 export default PhotoDetails
--- src/PhotoThumbnail.jsx
+++ src/PhotoThumbnail.jsx
@@ -1,57 +1,36 @@
 import './PhotoThumbnail.css'
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import PhotoDetails from './PhotoDetails';
 
-class PhotoThumbnail extends React.Component {
-  constructor(props) {
-    super(props); 
-    this.state = { showPhoto: false }
-    this.wrapperRef = React.createRef();
-    this.handleClickOutside = this.handleClickOutside.bind(this);
-    this.photo_thumbnail_url = this.props.photoData["s3_thumbnail_url"]
-  }
+function PhotoThumbnail(props) {
+  const [photoState, setPhotoState] = useState(false);
+  const photo_thumbnail_url = props.photoData["s3_thumbnail_url"]
+  const navigate = useNavigate();
 
-  componentDidMount() {
-    document.addEventListener("mousedown", this.handleClickOutside);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("mousedown", this.handleClickOutside);
-  }
-
-  handleClickOutside(event) {
-    if (this.wrapperRef && this.wrapperRef.current && !this.wrapperRef.current.contains(event.target)) {
-      this.showPhoto(false)
-    }
-  }
-
-  showPhoto = (bool) => {
-    this.setState({
-      showPhoto: bool
-    });
+  const showPhoto = (bool) => {
+    setPhotoState(bool)
     if (bool) {
       document.body.style.overflow='hidden'
+      navigate("/");
     } else {
       document.body.style.overflow='auto'
     }
   }
 
-  render() {
-    return (
-      <div style={{ height:"fit-content" }}>
-        <button class="photo-thumbnail" style={{ backgroundImage:"url(" + this.photo_thumbnail_url + ")" }} onClick={this.showPhoto.bind(null, true)}>
-          <div class="photo-thumbnail-overlay"></div>
-        </button>
-        { this.state.showPhoto && (
-          <div class="photo-details-wrapper">
-            <div class="photo-details-background"></div>
-            {/* ref={this.wrapperRef} below */}
-            <div><PhotoDetails photoData={this.props.photoData} showPhotoHandler={this.showPhoto}></PhotoDetails></div>
-          </div>
-        ) }
-      </div>
-    )
-  }
+  return (
+    <div style={{ height:"fit-content" }}>
+      <button class="photo-thumbnail" style={{ backgroundImage:"url(" + photo_thumbnail_url + ")" }} onClick={showPhoto.bind(null, true)}>
+        <div class="photo-thumbnail-overlay"></div>
+      </button>
+      { photoState && (
+        <div class="photo-details-wrapper">
+          <div class="photo-details-background"></div>
+          <div><PhotoDetails photoData={props.photoData} showPhotoHandler={showPhoto}></PhotoDetails></div>
+        </div>
+      ) }
+    </div>
+  )
 }
 
 export default PhotoThumbnail
